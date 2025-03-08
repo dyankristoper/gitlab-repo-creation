d074570a8dd4bdfe930b286c37cee0d9f1711cf6
John Christopher Azcarraga <stellarm_13@yahoo.com>
2025-03-08 23:56:51 +0800

    Fix: Removed secrets

--- src/group/group.js
+++ src/group/group.js
@@ -2,7 +2,7 @@ const axios = require("axios").default;
 const GITLAB_API = "https://gitlab.com/api/v4";
 const GROUPS_ENDPOINT = "/groups";
 const headers = {
-    Authorization: "Bearer glpat-v4oPy3dx6mt5VV8dxs4a",
+    Authorization: `Bearer ${ process.env.ACCESS_TOKEN }`,
   };
 
 const getSubgroups = async (groupId) => {
--- src/project/project.js
+++ src/project/project.js
@@ -4,7 +4,7 @@ const group = require("../group/group");
 const GITLAB_API = "https://gitlab.com/api/v4";
 const PROJECTS_ENDPOINT = "/projects";
 const headers = {
-  Authorization: "Bearer glpat-v4oPy3dx6mt5VV8dxs4a",
+  Authorization: `Bearer ${ process.env.ACCESS_TOKEN }`,
 };
 
 const createRepository = async (
